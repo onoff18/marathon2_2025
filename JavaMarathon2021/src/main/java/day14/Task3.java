6ca25146651b114f40c723094d83b9dad617ae36
onoff18 <maxvnik@gmail.com>
2025-07-23 03:50:31 +0300

    День 14
    Signed-off-by: onoff18 <maxvnik@gmail.com>

--- JavaMarathon2021/src/main/java/day14/Task3.java
+++ JavaMarathon2021/src/main/java/day14/Task3.java
@@ -1,7 +1,53 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        String path = "C:\\Users\\Alesha\\Desktop\\Java\\JavaMarathon2025\\JavaMarathon2021" +
+                "\\src\\main\\resources\\people.txt";
+
+        File file = new File(path);
+
+        System.out.println(parseFileToObjList(file));
+
+
+    }
+
+    public static List<Person> parseFileToObjList(File file) {
+
+        List<Person> persons = new ArrayList<>();
+
+        try {
+
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine();
+
+                if (Integer.parseInt(line.split(" ")[1]) < 0) {
+                    persons = null;
+                    throw new IllegalArgumentException();
+                }
+
+                persons.add(new Person(line.split(" ")[0], Integer.parseInt(line.split(" ")[1])));
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+
+        return persons;
 
     }
 }
